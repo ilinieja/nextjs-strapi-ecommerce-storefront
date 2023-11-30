272df2b62a04beb1b8a7bdf0bce0d4c686441d52
ilinieja <b2709f1b@gmail.com>
2023-11-30 21:09:15 +0100

    Remove locations and conditions pages

--- drone-rental-storefront-frontend/app/conditions/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Conditions() {
-    return <div>conditions</div>;
-}
--- drone-rental-storefront-frontend/components/Header/Header.tsx
+++ drone-rental-storefront-frontend/components/Header/Header.tsx
@@ -1,25 +1,26 @@
+'use client';
+
 import Link from "next/link";
 import clsx from "clsx";
 
 import SvgIconDrone from "../icons/SvgIconsDrone";
 import CartIcon from "../CartIcon/CartIcon";
+import { usePathname } from "next/navigation";
 
 const links = [
   {
-    title: "Drones",
+    title: "Store",
     href: "/store",
   },
   {
-    title: "Locations",
-    href: "/locations",
-  },
-  {
-    title: "Conditions",
-    href: "/conditions",
+    title: "Cart",
+    href: "/cart",
   },
 ];
 
 export default function Header({ className }: { className?: string }) {
+  const pathname = usePathname();
+
   return (
     <header className={clsx(className, "flex justify-center items-center border-b border-current backdrop-blur-md z-20 bg-light/70 dark:bg-dark/70")}>
       <div className="max-w-7xl w-full py-8 grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
@@ -30,7 +31,7 @@ export default function Header({ className }: { className?: string }) {
         </div>
         <nav className="col-span-2 md:col-span-6 lg:col-span-10 flex items-center gap-4">
           {links.map(({ title, href }) => (
-            <Link className="text-lg" href={href}>{title}</Link>
+            <Link className={clsx("text-lg", {"border-b": pathname === href})} href={href}>{title}</Link>
           ))}
         </nav>
         <div className="col-span-1 flex items-center justify-center">
